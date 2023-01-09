951a6cd8a359e4a45dae6f4eb644e0df59d36f9a
andre353 <eva595eva@gmail.com>
2023-01-09 21:41:08 +0300

    Add increment and decrement actions to reducer

--- src/components/Likes.jsx
+++ src/components/Likes.jsx
@@ -1,21 +1,37 @@
 import {connect} from 'react-redux'
 
 const Likes = (props) => {
+  console.log('render >', props);
   return (
     <div>
       <div className="button-controls">
-        <button>{props.likes}</button>
-        <button>❤ Dislike</button>
+        <button onClick={props.onIncrementLikes}>❤ {props.likes}</button>
+        <button onClick={props.onDecrementLikes}>Dislike</button>
       </div>
     </div>
   )
 };
-
+// так мы получаем доступ к state хранилища redux
 const mapStateToProps = (state) => {
   console.log('mapStateToProps >', state);
   return {
-    likes: state.likes
+    likes: state.likes // станет доступным в компоненте Likes через props
   }
-}
+};
 
-export default connect(mapStateToProps)(Likes)
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onIncrementLikes: () => { // станет доступным в компоненте Likes через props
+      console.log('click');
+      const action = {type: 'INCREMENT'};
+      dispatch(action); // так мы получаем наш action в reducer в store.js
+    },
+    onDecrementLikes: () => { // станет доступным в компоненте Likes через props
+      console.log('click decrement');
+      const action = {type: 'DECREMENT'};
+      dispatch(action); // так мы получаем наш action в reducer в store.js
+    }
+  }
+} 
+
+export default connect(mapStateToProps, mapDispatchToProps)(Likes)
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,10 +3,24 @@ import {createStore} from 'redux';
 const initialState = {
   likes: 10
 };
-
+// initialState записывается в state хранилища redux после начального запуска приложения, когда автоматически создается 1й action, который называется '@@redux/INITs.h.i.y.w.h', после этого данные приходят в компонент и он перерисовывается
 const reducer = (state = initialState, action) => {
   console.log('reducer', action);
-  return state;
+  // внутри reducer фиксируем каждый запуск dispatch
+  switch(action.type) {
+    case 'INCREMENT':
+      return {
+        ...state,
+        likes: state.likes + 1
+      }
+    case 'DECREMENT':
+      return {
+        ...state,
+        likes: state.likes - 1
+      }
+    default:
+      return state  
+  }
 };
 
 const store = createStore(reducer);
